48e3e16a0ae9b86d40658fae1fc572a1eccf4738
KevinKrummsdorf <kevin.krummsdorf1995@outlook.de>
2025-12-31 14:51:13 +0100

    update conig and index

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 declare(strict_types=1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once __DIR__ . '/../includes/config.inc.php';
 
 // ✅ Weiterleitung bei erfolgreichem Login + 2FA
